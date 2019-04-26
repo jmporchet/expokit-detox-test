adf10c791cca82176a73386ef95fc934b536a8a5
Jean-Marie <jm@jmporchet.ch>
2019-04-26 11:58:31 +0200

    fix: actually include a test that should pass

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.container} testID="welcome">
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
